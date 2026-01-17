47869fc3ba1e40233ce5ccc01b38e42ade80536a
czl <1848166725@qq.com>
2026-01-18 01:41:08 +0800

    run_libero

---
+++ run_libero.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 保存为 run_quick.sh
+# 最简单的版本，直接启动两个终端
+
+gnome-terminal -- bash -c "source examples/libero/.venv/bin/activate; export PYTHONPATH=\$PYTHONPATH:\$PWD/third_party/libero; python examples/libero/main.py; exec bash"
+gnome-terminal -- bash -c "source .venv/bin/activate; python scripts/serve_policy.py; exec bash"
