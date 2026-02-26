62ea13037966e9e1b429e77d8ecde102e97457b8
caozilin <1848166725@qq.com>
2026-02-26 16:08:53 +0800

    2.26

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -950,7 +950,7 @@ _CONFIGS = [
         name="pi05_libero",
         model=pi0_config.Pi0Config(pi05=True, action_horizon=10, discrete_state_input=False),
         data=LeRobotLiberoDataConfig(
-            repo_id="caozilin/libero",
+            repo_id="physical-intelligence/libero",
             base_config=DataConfig(prompt_from_task=True),
             extra_delta_transform=False,
         ),
